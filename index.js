a8ccaa01af253290e1a929f0158a2eb16868bf2d
G Sumanth <105142891+gsumanth101@users.noreply.github.com>
2026-05-31 15:01:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const { google } = require("googleapis");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.options("*", cors());
+
 app.use(express.json());
 
 const auth = new google.auth.GoogleAuth({
